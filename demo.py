1ecc4ad7ea2edf518b9a173e18ac4eeee341e0b1
Vikas-McW <vikas.prajapati@multicorewareinc.com>
2024-03-22 11:37:00 +0530

    python script

---
+++ demo.py
@@ -0,0 +1,47 @@
+import torch
+import pytorch_mask_rcnn as pmr
+
+
+use_cuda = True
+dataset = "coco"
+# ckpt_path = "../ckpts/maskrcnn_voc-5.pth"
+data_dir = "dataset\COCO\coco2017\val2017"
+
+device = torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu")
+if device.type == "cuda":
+    pmr.get_gpu_prop(show=True)
+print("\ndevice: {}".format(device))
+
+ds = pmr.datasets(dataset, data_dir, "val2017", train=True)
+#indices = torch.randperm(len(ds)).tolist()
+#d = torch.utils.data.Subset(ds, indices)
+data_loader = torch.utils.data.DataLoader(ds, shuffle=False)
+
+model = pmr.maskrcnn_resnet50(True, max(ds.classes) + 1).to(device)
+model.eval()
+model.head.score_thresh = 0.3
+
+"""
+if ckpt_path:
+    checkpoint = torch.load(ckpt_path, map_location=device)
+    model.load_state_dict(checkpoint["model"])
+    print(checkpoint["eval_info"])
+    del checkpoint
+"""
+for p in model.parameters():
+    p.requires_grad_(False)
+    
+iters = 5
+
+for i, (image, target) in enumerate(data_loader):
+    image = image.to(device)[0]
+    #target = {k: v.to(device) for k, v in target.items()}
+    
+    with torch.no_grad():
+        result = model(image)
+    
+    pmr.show(image, result, ds.classes, "./image/output{}.jpg".format(i))
+
+    if i >= iters - 1:
+        break
+
